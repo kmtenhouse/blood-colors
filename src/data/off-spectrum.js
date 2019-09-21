949ab80b5fcd607490bcadb224d07eec6c2b9aa4
Tassa Tenhouse <kmtenhouse@gmail.com>
2019-09-21 02:52:51 -0700

    Averaging colors for best difference

--- src/data/off-spectrum.js
+++ src/data/off-spectrum.js
@@ -1,7 +1,10 @@
 const offSpec = [
     ["FF0000", "Karkat"],
+    ["413C37", "grays"],
     ["000000", "black"],
-    ["FFFFFF", "white"]
+    ["FFFFFF", "white"],
+    ["b32d21", "terracotta"],
+    ["626649", "finch"]
 ];
 
 export default offSpec;
