f96323a34632cfaaa2447e95436ea04e639c7480
Tassa Tenhouse <kmtenhouse@gmail.com>
2019-09-22 00:00:00 -0700

    Added server

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# blood-colors
-A list of colors for designers, sorted by their place on the color wheel
---
+++ server/routes/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const router = require("express").Router();
+
+// If no API routes are hit, send the React app
+router.use((req, res) =>
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+);
+
+module.exports = router;
