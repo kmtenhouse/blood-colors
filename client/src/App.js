f5f0d10202f7d8101afb3b83228dd137e1d87372
Tassa Tenhouse <kmtenhouse@gmail.com>
2019-09-23 03:21:37 -0700

    Added new tier box

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 /* Import components */
 import Container from './components/Container';
 import Spectrum from './components/Spectrum';
+import Tier from './components/Tier';
 
 /* Import local data sources */
 
@@ -15,9 +16,33 @@ class App extends React.Component {
     //bindings!
     this.handleChange = this.handleChange.bind(this);
 
+    this.state = {
+      tiers: [],
+      colors: []
+    };
   }
 
   componentDidMount() {
+    //test data!
+    const fakeTier = {
+      name: "Rustie",
+      order: 0,
+      colors: [],
+      _id: "5d888c1b46a0a604a402e950",
+      displayColor: {
+        name: "Rust - Aradia Tier",
+        _id: "5d886724bd4b55584cbb1a8e",
+        hex: "#a10000",
+        contrastColor: "#FFFFFF",
+        date: "2019-09-23T09:10:51.642Z",
+        __v: 0
+      },
+      date: "2019-09-23T09:10:51.647Z",
+      __v: 0
+    };
+
+    this.setState({ tiers: [fakeTier] });
+
   }
 
   /*   Form Handling */
@@ -28,16 +53,19 @@ class App extends React.Component {
   }
 
   render() {
-
     return (
       <Container>
-
         <Spectrum title="Hemospectrum">
-          <h2>Test</h2>
+          {this.state.tiers.map(tier => (
+            <Tier key={tier._id} name={tier.name} color={tier.displayColor} >
+            </Tier>)
+          )}
         </Spectrum>
 
         <Spectrum title="Off Spectrum">
-          <h2>Test</h2>
+          <Tier>
+
+          </Tier>
         </Spectrum>
       </Container>
     );
--- client/src/components/Tier/index.js
+++ client/src/components/Tier/index.js
@@ -3,10 +3,20 @@ import './tier.css';
 
 
 function Tier(props) {
-  
+  const { color, name} = props;
+  console.log(color);
+  const tierColors = { 
+    backgroundColor: ( (color && color.hex) ? color.hex : "#000000"),
+    color: ( (color && color.contrastColor) ? color.contrastColor : "#FFFFFF")
+  };
+
+
   return (
     <div className="tier">
-
+      <div className="tier__header" style={tierColors}>
+        <h2 className="tier__title">{(name ? name : '')}</h2>
+      </div>
+      <div className="tier__collection">Collection goes here</div>
     </div>
   );
 }
